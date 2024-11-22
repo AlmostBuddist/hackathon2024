900c146408e937ec19dd8d88d81513538b8504a3
VladiSlave <bloodhomaq@gmail.com>
2024-11-23 02:08:35 +0300

    Поиск девайсов

--- frontent/src/App.tsx
+++ frontent/src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { defaultQuery } from "./url/axios";
 
 function App() {
+  const [isLoaging, setIsLoading] = useState<boolean>(false);
+
+  const onSearch = async () => {
+    try{
+
+    setIsLoading(true);
+
+    const res = await defaultQuery.get("/search_devices");
+
+    console.log(res);
+  } catch (err) {
+    const typesError = err;
+    console.log(err)
+  } finally {
+    setIsLoading(false)
+  }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={onSearch} disabled={isLoaging}>
+        Поиск устройств
+      </button>
+
+      {isLoaging && <div>Loading...</div>}
+    </>
   );
 }
 
--- server/app.py
+++ server/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request
+from flask import Flask, jsonify
+from neurosdk.scanner import Scanner
+from neurosdk.sensor import Sensor
+from neurosdk.cmn_types import *
+from time import sleep 
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-
+CORS(app, support_credentials=True)
 
 @app.route("/")
 def hello_world():
@@ -19,3 +24,28 @@ def test():
         return 'Login Proccesing'
     else:
         return 'Ну типа, ты вошёл'
+
+@app.route('/search_devices')
+def search_scaners():
+    def sensor_found(scanner, sensors):
+        print(sensors)
+        for sensor in sensors:
+            print('Sensor %s' % sensor)
+    
+    scanner = Scanner([SensorFamily.LEBrainBit])
+    scanner.sensorsChanged = sensor_found
+    scanner.start()
+    print("Starting search for 5 sec...")
+    sleep(5)
+    scanner.stop()
+
+    sensorsInfo = scanner.sensors()
+    
+    def toJson(sensor: SensorInfo):
+        return {
+            'name': sensor.Name,
+            'address': sensor.Address,
+            'serialNumber': sensor.SerialNumber,
+        }
+    
+    return jsonify(list(map(toJson, sensorsInfo)))
